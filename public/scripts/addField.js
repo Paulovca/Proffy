435f77e0ce489f8df2412992751cae50ad6327ef
Paulo Aureliano <paulo.c.aureliano@gmail.com>
2020-08-09 16:02:26 -0300

    so uns ajustes

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,16 +1,16 @@
 //procura o botao e quando clicar nele executa a função
-document.querySelector("#add-time").addEventListener("click", cloneField);
+document.querySelector("#add-time").addEventListener("click", cloneField)
 //função
 function cloneField() {
-    //duplica os campos quue retornou na busca
-    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true);
+    //duplica os campos que retornou na busca
+    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true)
     //pegar os campos
-    const fields = newFieldContainer.querySelectorAll("input");
+    const fields = newFieldContainer.querySelectorAll("input")
     //para cada campo, limpar
     fields.forEach(function (field) {
-        field.value = "";
+        field.value = ""
     });
     
     //local para posicionar os campos
-    document.querySelector("#schedule-items").appendChild(newFieldContainer);
+    document.querySelector("#schedule-items").appendChild(newFieldContainer)
 }
